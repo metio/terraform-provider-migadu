72d1b2e7b2dda1804b110c6e3c6f01914715d418
Sebastian Hoß <seb@xn--ho-hia.de>
2023-01-09 08:00:09 +0100

    add terratest for migadu_identity data source

---
+++ terratest/data-sources/migadu_identity/data-source.tf
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: The terraform-provider-git Authors
+# SPDX-License-Identifier: 0BSD
+
+data "migadu_identity" "identity" {
+  domain_name = var.domain_name
+  local_part  = var.local_part
+  identity    = var.identity
+}
