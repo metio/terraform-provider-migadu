5974c23e2131f70a7c656ab5ef4b9ca43bfaf68d
Sebastian Hoß <seb@xn--ho-hia.de>
2023-02-28 07:47:52 +0100

    add terratest for migadu_rewrite data source

---
+++ terratest/data-sources/migadu_rewrite/variables.tf
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: The terraform-provider-migadu Authors
+# SPDX-License-Identifier: 0BSD
+
+variable "endpoint" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
