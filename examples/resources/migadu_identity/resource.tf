cf01c23b7101b4990f260e7cde910d5bed8552fd
Sebastian Hoß <seb@xn--ho-hia.de>
2024-02-10 05:46:34 +0100

    allow to specify 'password_use'
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

--- examples/resources/migadu_identity/resource.tf
+++ examples/resources/migadu_identity/resource.tf
@@ -10,3 +10,28 @@ resource "migadu_identity" "idn" {
   local_part  = "some-mailbox"
   identity    = "some-identity"
 }
+
+# Sender persona, not used for authentication
+resource "migadu_identity" "sender_persona" {
+  domain_name  = "example.com"
+  local_part   = "some-mailbox"
+  identity     = "some-identity"
+  password_use = "none" # use mailbox user/password
+}
+
+# alternative address linked to the same mailbox
+resource "migadu_identity" "mailbox" {
+  domain_name  = "example.com"
+  local_part   = "some-mailbox"
+  identity     = "some-identity"
+  password_use = "mailbox" # use mailbox password
+}
+
+# application specific password
+resource "migadu_identity" "custom" {
+  domain_name  = "example.com"
+  local_part   = "some-mailbox"
+  identity     = "some-identity"
+  password_use = "custom" # use custom user/password
+  password     = "Sup3r_s3cr3T"
+}
