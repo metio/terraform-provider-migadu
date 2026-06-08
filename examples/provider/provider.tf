197555d5f0bdbc082b2de81f1904b9aec96c1d55
pasmophobia <l@leonmatsuistroemsvik.com>
2026-06-09 04:14:03 +0900

    Add examples and documentation for client-side rate limiting configuration

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -9,3 +9,18 @@ provider "migadu" {
   timeout  = 35
   endpoint = "https://api.migadu.com/v1/"
 }
+
+# override the default rate limit (defaults to 60 requests per 2m)
+provider "migadu" {
+  username      = "some-name@example.com"
+  token         = "your-super-secret-token-that-should-not-be-committed-in-plaintext"
+  rate_limit    = 30
+  rate_interval = "1m"
+}
+
+# disable client-side rate limiting
+provider "migadu" {
+  username   = "some-name@example.com"
+  token      = "your-super-secret-token-that-should-not-be-committed-in-plaintext"
+  rate_limit = 0
+}
